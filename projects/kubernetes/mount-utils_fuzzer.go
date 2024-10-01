e51f57c1f2c5b6a1fc9f73b3e51d27df0ad8efed
wattmto <dev@wattmto.dev>
2024-10-01 23:07:00 +0900

    fixup! Rewrite tests with native fuzzer

--- projects/kubernetes/mount-utils_fuzzer.go
+++ projects/kubernetes/mount-utils_fuzzer.go
@@ -15,23 +15,25 @@
 
 // place in kubernetes/staging/src/k8s.io/mount-utils
 package mount
-  
+
 import (
-        "io/ioutil"
-        "os"
+	"os"
+	"testing"
 )
 
-func FuzzSearchMountPoints(data []byte) int {
-        tmpFile, err := ioutil.TempFile("", "test-get-filetype")
-        if err != nil {
-                panic(err)
-        }
-        defer os.Remove(tmpFile.Name())
-        defer tmpFile.Close()
-        tmpFile.Truncate(0)
-        tmpFile.Seek(0, 0)
-        tmpFile.WriteString(string(data))
-        tmpFile.Sync()
-        _, _ = SearchMountPoints("/mnt/disks/vol1", tmpFile.Name())
-        return 1
+func FuzzSearchMountPoints(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		tmpFile, err := os.CreateTemp("", "test-get-filetype")
+		if err != nil {
+			panic(err)
+		}
+		defer os.Remove(tmpFile.Name())
+		defer tmpFile.Close()
+		tmpFile.Truncate(0)
+		tmpFile.Seek(0, 0)
+		tmpFile.WriteString(string(data))
+		tmpFile.Sync()
+		_, _ = SearchMountPoints("/mnt/disks/vol1", tmpFile.Name())
+		return
+	})
 }
