6ab8cb5a4b96c9215d9364fa96f8633ba70de842
wattmto <dev@wattmto.dev>
2024-10-03 08:15:21 +0900

    fixup! Rewrite tests with native fuzzer

--- projects/kubernetes/gofuzz-fuzzers.go
+++ projects/kubernetes/gofuzz-fuzzers.go
@@ -44,66 +44,71 @@ func initForFuzzing() {
 	testing.Init()
 }
 
-func FuzzRoundTripSpecificKind(data []byte) int {
-	initter.Do(initForFuzzing)
-	t := &testing.T{}
-	internalGVK := schema.GroupVersionKind{Group: "apps", Version: runtime.APIVersionInternal, Kind: "DaemonSet"}
+func FuzzRoundTripSpecificKind(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		initter.Do(initForFuzzing)
+		internalGVK := schema.GroupVersionKind{Group: "apps", Version: runtime.APIVersionInternal, Kind: "DaemonSet"}
 
-	seed := bytesource.New(data)
-	fuzzer := fuzzer.FuzzerFor(apitesting.FuzzerFuncs, seed, legacyscheme.Codecs)
+		seed := bytesource.New(data)
+		fuzzer := fuzzer.FuzzerFor(apitesting.FuzzerFuncs, seed, legacyscheme.Codecs)
 
-	roundtrip.RoundTripSpecificKind(t, internalGVK, legacyscheme.Scheme, legacyscheme.Codecs, fuzzer, nil)
-	return 1
+		roundtrip.RoundTripSpecificKind(t, internalGVK, legacyscheme.Scheme, legacyscheme.Codecs, fuzzer, nil)
+		return
+	})
 }
 
-func FuzzControllerRoundtrip(data []byte) int {
-	initter.Do(initForFuzzing)
-	t := &testing.T{}
+func FuzzControllerRoundtrip(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		initter.Do(initForFuzzing)
 
-	seed := bytesource.New(data)
-	f := fuzzer.FuzzerFor(controllerFuzzer.Funcs, seed, legacyscheme.Codecs)
+		seed := bytesource.New(data)
+		fuzzer := fuzzer.FuzzerFor(controllerFuzzer.Funcs, seed, legacyscheme.Codecs)
 
-	codecFactory := runtimeserializer.NewCodecFactory(controllerScheme.Scheme)
-	roundtrip.RoundTripTypesWithoutProtobuf(t, controllerScheme.Scheme, codecFactory, f, nil)
-	return 1
+		codecFactory := runtimeserializer.NewCodecFactory(controllerScheme.Scheme)
+		roundtrip.RoundTripTypesWithoutProtobuf(t, controllerScheme.Scheme, codecFactory, fuzzer, nil)
+		return
+	})
 }
 
-func FuzzKubeletSchemeRoundtrip(data []byte) int {
-	initter.Do(initForFuzzing)
-	t := &testing.T{}
+func FuzzKubeletSchemeRoundtrip(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		initter.Do(initForFuzzing)
 
-	seed := bytesource.New(data)
-	f := fuzzer.FuzzerFor(kubeletFuzzer.Funcs, seed, legacyscheme.Codecs)
+		seed := bytesource.New(data)
+		fuzzer := fuzzer.FuzzerFor(kubeletFuzzer.Funcs, seed, legacyscheme.Codecs)
 
-	klScheme, _, err := kubeletScheme.NewSchemeAndCodecs()
-	if err != nil {
-		return 0
+		klScheme, _, err := kubeletScheme.NewSchemeAndCodecs()
+		if err != nil {
+			return
+		}
+		codecFactory := runtimeserializer.NewCodecFactory(klScheme)
+		roundtrip.RoundTripTypesWithoutProtobuf(t, klScheme, codecFactory, fuzzer, nil)
+		return
 	}
-	codecFactory := runtimeserializer.NewCodecFactory(klScheme)
-	roundtrip.RoundTripTypesWithoutProtobuf(t, klScheme, codecFactory, f, nil)
-	return 1
 }
 
-func FuzzProxySchemeRoundtrip(data []byte) int {
-	initter.Do(initForFuzzing)
-	t := &testing.T{}
+func FuzzProxySchemeRoundtrip(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		initter.Do(initForFuzzing)
 
-	seed := bytesource.New(data)
-	f := fuzzer.FuzzerFor(proxyFuzzer.Funcs, seed, legacyscheme.Codecs)
+		seed := bytesource.New(data)
+		fuzzer := fuzzer.FuzzerFor(proxyFuzzer.Funcs, seed, legacyscheme.Codecs)
 
-	codecFactory := runtimeserializer.NewCodecFactory(proxyScheme.Scheme)
-	roundtrip.RoundTripTypesWithoutProtobuf(t, proxyScheme.Scheme, codecFactory, f, nil)
-	return 1
+		codecFactory := runtimeserializer.NewCodecFactory(proxyScheme.Scheme)
+		roundtrip.RoundTripTypesWithoutProtobuf(t, proxyScheme.Scheme, codecFactory, fuzzer, nil)
+		return
+	})
 }
 
-func FuzzRoundTripType(data []byte) int {
-	initter.Do(initForFuzzing)
-	t := &testing.T{}
+func FuzzRoundTripType(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte){
+		initter.Do(initForFuzzing)
 
-	seed := bytesource.New(data)
-	f := fuzzer.FuzzerFor(apitesting.FuzzerFuncs, seed, legacyscheme.Codecs)
-	nonRoundTrippableTypes := map[schema.GroupVersionKind]bool{}
+		seed := bytesource.New(data)
+		fuzzer := fuzzer.FuzzerFor(apitesting.FuzzerFuncs, seed, legacyscheme.Codecs)
+		nonRoundTrippableTypes := map[schema.GroupVersionKind]bool{}
 
-	roundtrip.RoundTripTypes(t, legacyscheme.Scheme, legacyscheme.Codecs, f, nonRoundTrippableTypes)
-	return 1
+		roundtrip.RoundTripTypes(t, legacyscheme.Scheme, legacyscheme.Codecs, fuzzer, nonRoundTrippableTypes)
+		return
+	})
 }
