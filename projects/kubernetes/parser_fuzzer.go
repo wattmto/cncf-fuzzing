b5f6067e6241ac6de72d8b1c78645c18d303c0b8
wattmto <dev@wattmto.dev>
2024-10-03 09:02:38 +0900

    fixup! Rewrite tests with native fuzzer

--- projects/kubernetes/parser_fuzzer.go
+++ projects/kubernetes/parser_fuzzer.go
@@ -17,6 +17,7 @@ package fuzzing
 
 import (
 	"io"
+	"testing"
 
 	fuzz "github.com/AdaLogics/go-fuzz-headers"
 
@@ -32,50 +33,62 @@ import (
 	kubeadmutil "k8s.io/kubernetes/cmd/kubeadm/app/util"
 	"k8s.io/kubernetes/pkg/credentialprovider"
 	"k8s.io/kubernetes/pkg/kubelet/cm"
-	"k8s.io/utils/cpuset"
 	"k8s.io/kubernetes/pkg/util/parsers"
+	"k8s.io/utils/cpuset"
 )
 
 // FuzzParseQuantity implements a fuzzer
 // that targets resource.ParseQuantity
-func FuzzParseQuantity(data []byte) int {
-	_, _ = resource.ParseQuantity(string(data))
-	return 1
+func FuzzParseQuantity(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		_, _ = resource.ParseQuantity(string(data))
+		return
+	})
 }
 
 // FuzzMeta1ParseToLabelSelector implements a fuzzer
 // that targets metav1.ParseToLabelSelector
-func FuzzMeta1ParseToLabelSelector(data []byte) int {
-	_, _ = metav1.ParseToLabelSelector(string(data))
-	return 1
+func FuzzMeta1ParseToLabelSelector(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		_, _ = metav1.ParseToLabelSelector(string(data))
+		return
+	})
 }
 
 // FuzzParseSelector implements a fuzzer
 // that targets fields.ParseSelector
-func FuzzParseSelector(data []byte) int {
-	_, _ = fields.ParseSelector(string(data))
-	return 1
+func FuzzParseSelector(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		_, _ = fields.ParseSelector(string(data))
+		return
+	})
 }
 
 // FuzzLabelsParse implements a fuzzer
 // that targets labels.Parse
-func FuzzLabelsParse(data []byte) int {
-	_, _ = labels.Parse(string(data))
-	return 1
+func FuzzLabelsParse(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		_, _ = labels.Parse(string(data))
+		return
+	})
 }
 
 // FuzzParseGroupVersion implements a fuzzer
 // that targets schema.ParseGroupVersion
-func FuzzParseGroupVersion(data []byte) int {
-	_, _ = schema.ParseGroupVersion(string(data))
-	return 1
+func FuzzParseGroupVersion(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		_, _ = schema.ParseGroupVersion(string(data))
+		return
+	})
 }
 
 // FuzzParseResourceArg implements a fuzzer
 // that targets schema.ParseResourceArg
-func FuzzParseResourceArg(data []byte) int {
-	_, _ = schema.ParseResourceArg(string(data))
-	return 1
+func FuzzParseResourceArg(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		_, _ = schema.ParseResourceArg(string(data))
+		return
+	})
 }
 
 // FuzzParseVersion implements a fuzzer
@@ -84,136 +97,156 @@ func FuzzParseResourceArg(data []byte) int {
 // - version/(*Version).String()
 // - version.ParseGeneric
 // - version/(*Version).AtLeast(*Version)
-func FuzzParseVersion(data []byte) int {
-	f := fuzz.NewConsumer(data)
-	vString1, err := f.GetString()
-	if err != nil {
-		return 0
-	}
-	v1, err := version.ParseSemantic(vString1)
-	if err != nil {
-		return 0
-	}
+func FuzzParseVersion(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		fdp := fuzz.NewConsumer(data)
+		vString1, err := fdp.GetString()
+		if err != nil {
+			return
+		}
+		v1, err := version.ParseSemantic(vString1)
+		if err != nil {
+			return
+		}
 
-	// Test if the Version will crash (*Version).String()
-	_ = v1.String()
+		// Test if the Version will crash (*Version).String()
+		_ = v1.String()
 
-	vString2, err := f.GetString()
-	if err != nil {
-		return 0
-	}
-	v2, err := version.ParseGeneric(vString2)
-	if err != nil {
-		return 0
-	}
-	_ = v1.AtLeast(v2)
-	return 1
+		vString2, err := fdp.GetString()
+		if err != nil {
+			return
+		}
+		v2, err := version.ParseGeneric(vString2)
+		if err != nil {
+			return
+		}
+		_ = v1.AtLeast(v2)
+		return
+	})
 }
 
 // FuzzParsePrivateKeyPEM implements a fuzzer
 // that targets keyutil.ParsePrivateKeyPEM
-func FuzzParsePrivateKeyPEM(data []byte) int {
-	_, _ = keyutil.ParsePrivateKeyPEM(data)
-	return 1
+func FuzzParsePrivateKeyPEM(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		_, _ = keyutil.ParsePrivateKeyPEM(data)
+		return
+	})
 }
 
 // FuzzParsePublicKeysPEM implements a fuzzer
 // that targets keyutil.ParsePublicKeysPEM
-func FuzzParsePublicKeysPEM(data []byte) int {
-	_, _ = keyutil.ParsePublicKeysPEM(data)
-	return 1
+func FuzzParsePublicKeysPEM(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		_, _ = keyutil.ParsePublicKeysPEM(data)
+		return
+	})
 }
 
 // FuzzParseHostPort implements a fuzzer
 // that targets kubeadmutil.ParseHostPort
-func FuzzParseHostPort(data []byte) int {
-	_, _, _ = kubeadmutil.ParseHostPort(string(data))
-	return 1
+func FuzzParseHostPort(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		_, _, _ = kubeadmutil.ParseHostPort(string(data))
+		return
+	})
 }
 
 // FuzzUrlsMatch implements a fuzzer
 // that targets credentialprovider.URLsMatchStr
-func FuzzUrlsMatch(data []byte) int {
-	f := fuzz.NewConsumer(data)
-	glob, err := f.GetString()
-	if err != nil {
-		return 0
-	}
-	target, err := f.GetString()
-	if err != nil {
-		return 0
-	}
-	_, _ = credentialprovider.URLsMatchStr(glob, target)
-	return 1
+func FuzzUrlsMatch(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		fdp := fuzz.NewConsumer(data)
+		glob, err := fdp.GetString()
+		if err != nil {
+			return
+		}
+		target, err := fdp.GetString()
+		if err != nil {
+			return
+		}
+		_, _ = credentialprovider.URLsMatchStr(glob, target)
+		return
+	})
 }
 
 // FuzzParseCSR implements a fuzzer
 // that targets certificate.ParseCSR
-func FuzzParseCSR(data []byte) int {
-	_, _ = certificate.ParseCSR(data)
-	return 1
+func FuzzParseCSR(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		_, _ = certificate.ParseCSR(data)
+		return
+	})
 }
 
 // FuzzParseEnv implements a fuzzer
 // that targets envutil.ParseEnv
-func FuzzParseEnv(data []byte) int {
-	f := fuzz.NewConsumer(data)
+func FuzzParseEnv(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		fdp := fuzz.NewConsumer(data)
 
-	// Create a pseudo-random spec.
-	// Will be used as argument to the fuzz target
+		// Create a pseudo-random spec.
+		// Will be used as argument to the fuzz target
 
-	// length of slice:
-	qty, err := f.GetInt()
-	if err != nil {
-		return 0
-	}
-	spec := make([]string, qty, qty)
-
-	// fill slice with values
-	for i := 0; i < qty; i++ {
-		s, err := f.GetString()
+		// length of slice:
+		qty, err := fdp.GetInt()
 		if err != nil {
-			return 0
+			return
 		}
-		spec = append(spec, s)
-	}
-	var r io.Reader
-	_, _, _, _ = envutil.ParseEnv(spec, r)
-	return 1
+		spec := make([]string, qty, qty)
+
+		// fill slice with values
+		for i := 0; i < qty; i++ {
+			s, err := fdp.GetString()
+			if err != nil {
+				return
+			}
+			spec = append(spec, s)
+		}
+		var r io.Reader
+		_, _, _, _ = envutil.ParseEnv(spec, r)
+		return
+	})
 }
 
 // FuzzParseQOSReserve implements a fuzzer
 // that targets cm.ParseQOSReserved
-func FuzzParseQOSReserve(data []byte) int {
-	f := fuzz.NewConsumer(data)
+func FuzzParseQOSReserve(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		fdp := fuzz.NewConsumer(data)
 
-	// Create a pseudo-random map.
-	// Will be used as argument to the fuzz target
-	m := make(map[string]string)
-	err := f.FuzzMap(&m)
-	if err != nil {
-		return 0
-	}
-	_, _ = cm.ParseQOSReserved(m)
-	return 1
+		// Create a pseudo-random map.
+		// Will be used as argument to the fuzz target
+		m := make(map[string]string)
+		err := fdp.FuzzMap(&m)
+		if err != nil {
+			return
+		}
+		_, _ = cm.ParseQOSReserved(m)
+		return
+	})
 }
 
 // FuzzParseCPUSet implements a fuzzer
 // that targets:
 // - cpuset.Parse
 // - cpuset/(CPUSet).String
-func FuzzParseCPUSet(data []byte) int {
-	cs, err := cpuset.Parse(string(data))
-	if err != nil {
-		return 0
-	}
-	_ = cs.String()
-	return 1
+func FuzzParseCPUSet(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		cs, err := cpuset.Parse(string(data))
+		if err != nil {
+			return
+		}
+		_ = cs.String()
+		return
+	})
 }
 
 // FuzzParseImageName implements a fuzzer
 // that targets parsers.ParseImageName
-func FuzzParseImageName(data []byte) int {
-	_, _, _, _ = parsers.ParseImageName(string(data))
-	return 1
+func FuzzParseImageName(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		_, _, _, _ = parsers.ParseImageName(string(data))
+		return
+	})
 }
